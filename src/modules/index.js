4bd0505e1f09521f03d43c2af2c4b561fb5a4257
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-08-19 21:56:23 +0200

    Add basic styles

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,3 +1,6 @@
 //https://www.npmjs.com/package/regenerator-runtime
 //runtime import - thanks to this we can use async function
 import regeneratorRuntime from "regenerator-runtime";
+
+//styles
+import "../styles/main.css";
