229ae9a4a291c1977f5488ae23319d341f5462f3
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-08-22 14:50:39 +0200

    Function responsible for "year" value in storege
    Info about year is required to define if it is a new year and if it is the
    app will reset all the inputs, in order to user has possibility to check
    the another day of his activity in new year

---
+++ src/modules/new_year_reset/newYearReset.js
@@ -0,0 +1,13 @@
+//If it is 1st run of app the function will set an info about current year, each time the application is started again, an another function checks if it is still the same year, if the year is different, it will replace the value of "year" in storage and clear the selected inputs
+function setYearWhen1stAppRun(){
+    //Boolien value which define if there an appropriate item in storage
+    const isYearInStorage = !!window.localStorage.getItem("year");
+    //If there is a "year" in storage that means the app is lunched not 1st time and then this function does nothing
+    if(isYearInStorage){
+        return;
+    }else{
+        //In other case(when it is 1st run of app) thr app has to set "year" value in storage, so that app can determine if it is new year, which means the need to reset the inputs
+        const year = new Date().getFullYear();
+        window.localStorage.setItem("year", year);
+    }
+}
