589b6d4c1cf9007d2b2c0f820557d485c85be24f
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-08-23 15:35:55 +0200

    Add function returnes current day of year
    This function will help with assigning the appropriate event hander to
    appropriate input. Only input represent this current day will have
    hander which update the input state in localstorage, each another input
    which represents not today will have hander which shows informations
    about not allowed operation

--- src/modules/state_handling/updateInputsState.js
+++ src/modules/state_handling/updateInputsState.js
@@ -1,4 +1,17 @@
 //This module is responsible for updating inputs status
+
+function getCurrentDayOfYear(){//https://stackoverflow.com/questions/8619879/javascript-calculate-the-day-of-the-year-1-366
+    const now = new Date();
+    const start = new Date(now.getFullYear(), 0, 0);
+    const diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+    const oneDay = 1000 * 60 * 60 * 24;
+    //must be -1 cuz value represent 1st day of january has index === 0, not one(cuzx it is an array) in order to "match" number of day and array index there must be -1
+    const day = Math.floor(diff / oneDay)-1;
+
+    return day;
+}
+
+
 function updateInputsState(){
     const arrayOfInputsState = JSON.parse(window.localStorage.getItem("inputsState"));
     const inputsFromPage = document.querySelectorAll(".calendar__input");
