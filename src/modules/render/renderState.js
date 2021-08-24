641a88c6edce52b0804e94da1c7870bc6d2db20f
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-08-24 16:27:40 +0200

    Transfer function between modules
    Function which is related with adding styles to element fit more to
    renderState than updateInputState

--- src/modules/render/renderState.js
+++ src/modules/render/renderState.js
@@ -17,7 +17,19 @@ function renderInputs(){
         const arrayOfInputsState = JSON.parse(window.localStorage.getItem("inputsState"));
         //Get all inputs from page
         const inputsFromPage = document.querySelectorAll(".calendar__input");
+        //This vatiable keeps information about current day of a year, ex if it is 2nd of jan it equals to 1, cuz I use this value in loops with arrays, and index of array starts form 0, look getCurrentDayOfYear function
         const howMuchDaysPassed = getCurrentDayOfYear();
+
+        //Inputs represent not today have diffrent styles in order to show user that they are disabled
+        inputsFromPage.forEach((input, inputIndex)=>{
+            //If it is current day, do not add special class, this day has to be clickabe
+            if(inputIndex === howMuchDaysPassed){
+                return;
+            }else{
+                //Reference to parent element, look DOM (input has "calendar__input" class)
+                input.parentElement.classList.add("calendar__day--blank");
+            }
+        });
         
         //Loop throught all values from localstorage which represent the past days(thanks to this this function will not add an appropriate class to these inputs which are future and which user was not able to checked), if this value equals to 1 that means this day is "checked"
         for(let i = 0; i <= howMuchDaysPassed; i++){
