770533d2c674c1581a514098500dd922ca6c352d
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-08-24 21:24:38 +0200

    Add handling of counting done habits
    Now this function is able to count and show this information to user

--- src/modules/show_number_of_checked_inputs/t.js
+++ src/modules/show_number_of_checked_inputs/t.js
@@ -5,7 +5,20 @@ function showNumberOfCheckedInputs(){
 
     for(const month of monthsElement){
         const inputsInMonths = month.querySelectorAll(".calendar__input");
-        console.log(inputsInMonths);
+        //Each month has special place when this information will be shown
+        const placeToShowAmount = month.querySelector(".calendar__userActiveCount");
+
+        //Iterator for number of "checked" inputs
+        let i = 0;
+        //Loop throught all inputs in month which helps to determine how many of inputs has checked === true
+        for(const input of inputsInMonths){
+            const isChecked = input.checked;
+            if(isChecked){
+                i++;
+            }
+        }
+        //After counting all inputs with checked === true in month show it in top right corner of month container
+        placeToShowAmount.innerHTML = i;
     }
 }
-export default show;
+export default showNumberOfCheckedInputs;
