e737574e4594d9d193ef9866630933ee68e807d6
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-08-24 16:53:26 +0200

    Change the name of folder
    By inattention I used camelCase to name of folder,
    so far i used notation with _ so I change it in order to to keep order

--- src/modules/index.js
+++ src/modules/index.js
@@ -6,10 +6,10 @@ import regeneratorRuntime from "regenerator-runtime";
 import "../styles/main.css";
 
 //modules import
-import setYearWhen1stAppRun from "./1stAppRun/setYear";
+import setYearWhen1stAppRun from "./1st_app_run/setYear";
 import leapYearExtraDay from "./feb_leap_year/febLeapYear";
 import clearIfItIsDiffrentYear from "./new_year_reset/newYearReset";
-import setBasicStateWhen1stRun from "./1stAppRun/setBasicStateWhen1stRun";
+import setBasicStateWhen1stRun from "./1st_app_run/setBasicStateWhen1stRun";
 import updateInputsState from "./state_handling/updateInputsState";
 import renderInputs from "./render/renderState";
 
