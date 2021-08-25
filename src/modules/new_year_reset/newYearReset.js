8b3d999476f9763b11f887833368bb0a6fca184a
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-08-25 21:15:22 +0200

    Removing appropriate classes
    Now when I know the class which checked/unchecked inputs have, I can
    remove it from inputs when it is new year

--- src/modules/new_year_reset/newYearReset.js
+++ src/modules/new_year_reset/newYearReset.js
@@ -20,7 +20,8 @@ function clearIfItIsDiffrentYear(){
         //The app has to get reference to these inputs here, in case before when this reference was in index.js, there was a problem with 29th of febuary - it does not exist in this element collection, cuz 29th of febuary was added later then the reference was obtained
         const monthInputsToClean = document.querySelectorAll(".calendar__input");
         for(const input of monthInputsToClean){
-            //Here will be code responsible for reseting the inputs, but I am not able to write it now, cuz I do not write code responsible for handling input state(whether it was checked or not) and I do not know which classes this function has to remove from each input
+            input.classList.remove("calendar__day--checked");
+            input.classList.remove("calendar__day--unchecked");
         }
     }
 }
