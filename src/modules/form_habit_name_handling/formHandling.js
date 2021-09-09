ce6f2967a1074826d7f4c531b98ff45ab68a82a8
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-09-09 17:01:11 +0200

    Save the habit name on the firebase
    Now all logic related with saving and showing the habit name is moved to
    the firebase database

--- src/modules/form_habit_name_handling/formHandling.js
+++ src/modules/form_habit_name_handling/formHandling.js
@@ -1,10 +1,17 @@
 // This simple module "listing" to change event on input, and set new habit name in storage each time detects change. habitName will be shown by default the next time when the app is started
+
+//Import firebase(this is file with firebase config)
+import app from "../firebase";
+
 function formHandling(){
     const form = document.querySelector("#habitForm");
+    //Get the refference to table on firebase
+    const habitNameOnDatabase = app.database().ref();
 
     form.addEventListener("change", ({target})=>{
-        const habitName = target.value;
-        window.localStorage.setItem("habitName", habitName);
+        const habitNameToSave = target.value;
+        //Update the element of table from firebase
+        habitNameOnDatabase.update({habitName: habitNameToSave});
     })
 }
 
