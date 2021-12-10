782128f2673852913df2ad65172339351c68b951
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-12-10 20:17:11 +0100

    Save app content to localstorage
    The next step is create logic which will generate the read-only content
    on view subpage

--- src/modules/createPreview/previewPage.js
+++ src/modules/createPreview/previewPage.js
@@ -1,5 +1,7 @@
 function previewPage(){
-    console.log(1);
+    const app = document.querySelector("#app");
+    //Each time update the localstorage, and based on this localstorage value, the content on view subpage will be generated(the content on this page will be read-only - it cannot be edited)
+    window.localStorage.setItem("content", app.innerHTML);
 }
 
 export default previewPage;
