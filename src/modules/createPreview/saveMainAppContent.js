7ebbda0ed441468b4eb9a0b118ceabef5f8efb31
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-12-19 21:57:53 +0100

    Use console.log in saveMainAppContent
    This function had to save app content after load event, so I console.log
    some values, but I do not see anything in console

--- src/modules/createPreview/saveMainAppContent.js
+++ src/modules/createPreview/saveMainAppContent.js
@@ -2,7 +2,9 @@ function saveMainAppContent(){
     const app = document.querySelector("#app");
 
     //Wait until the moment everything will be loaded
+    console.log(1)
     window.addEventListener("load", ()=>{
+        console.log("2")
         window.localStorage.setItem("content", app.innerHTML); 
     });
 }
